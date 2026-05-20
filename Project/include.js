873c0428cec1acbb6406bc46228f428edd9aaaca
deepa657 <deepamajhi.9860@gmail.com>
2026-05-20 09:07:52 +0545

    Changes in loginpage and Folders

--- Project/include.js
+++
@@ -1,8 +0,0 @@
-async function loadComponent() {
-    const response = await fetch(file);
-    const data = await response.text();
-    document.getElementById("component").innerHTML = data;
-}
-
-loadComponent("header","./header.html");
-loadComponent("footer","./footer.html");
