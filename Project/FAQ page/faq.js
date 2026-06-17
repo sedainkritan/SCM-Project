744d0bc993510f8f88286b077a343ba63c08d578
kritan-sedain <sedainkritan65@gmail.com>
2026-06-17 11:35:28 +0545

    Changes to the file name

--- Project/FAQ page/faq.js
+++
@@ -1,6 +0,0 @@
-const faqItems = document.querySelectorAll('.faq-item');
-    faqItems.forEach(item => {
-      item.querySelector('.faq-question').addEventListener('click', () => {
-        item.classList.toggle('active');
-      });
-    });
